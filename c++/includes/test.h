986ca8f75d41e0db36455b4b53ed63ec23d2dfff
Ivana <ivana.gyro@gmail.com>
2020-12-01 15:17:36 +0800

    Format header files

--- c++/includes/test.h
+++ c++/includes/test.h
@@ -1,35 +1,35 @@
 #ifndef TEST_H
 #define TEST_H
 
+#include <cassert>
 #include <initializer_list>
 #include <iostream>
-#include <cassert>
+
 #include "show.h"
 
-
-#define test(proc, ans...) \
-    std::cout << "test " << CASE_NO++ << ": "#proc << std::endl; \
-    std::cout << "answer: "#ans << std::endl; \
-    _test(proc, ans); \
+#define test(proc, ans...)                                        \
+    std::cout << "test " << CASE_NO++ << ": " #proc << std::endl; \
+    std::cout << "answer: " #ans << std::endl;                    \
+    _test(proc, ans);                                             \
     std::cout << "-----" << std::endl;
 
-int CASE_NO {1};
+int CASE_NO{1};
 
-template<typename T, typename V>
+template <typename T, typename V>
 void _test(const T& container, std::initializer_list<V> list) {
     std::cout << "input: " << container << std::endl;
     assert(container.size() == list.size());
-    auto begin_c {container.begin()};
-    auto begin_l {list.begin()};
+    auto begin_c{container.begin()};
+    auto begin_l{list.begin()};
     while (begin_c != container.end()) {
         assert(*begin_c++ == *begin_l++);
     }
 }
 
-template<typename T>
+template <typename T>
 void _test(const T& a, const T& b) {
     std::cout << "input: " << a << std::endl;
     assert(a == b);
 }
 
-#endif // TEST_H
+#endif  // TEST_H
--- c++/includes/tree.h
+++ c++/includes/tree.h
@@ -5,18 +5,16 @@
 #include <queue>
 
 template <typename T, typename Node>
-class _Node
-{
-public:
+class _Node {
+   public:
     _Node(void *ptr) : ptr{nullptr} {}
     _Node(const int &val) : ptr{new Node(val)} {}
-    Node* ptr;
+    Node *ptr;
 };
 
 template <typename T>
-class TreeNode
-{
-public:
+class TreeNode {
+   public:
     TreeNode() : val{} {}
     TreeNode(const T &val) : val{val} {}
     TreeNode(const T &val, TreeNode *left, TreeNode *right)
@@ -27,12 +25,12 @@ public:
 };
 
 template <typename T>
-TreeNode<T>* build_tree(std::initializer_list<_Node<T, TreeNode<T>>> il) {
-    TreeNode<T>* root {};
-    std::queue<TreeNode<T>**> q;
+TreeNode<T> *build_tree(std::initializer_list<_Node<T, TreeNode<T>>> il) {
+    TreeNode<T> *root{};
+    std::queue<TreeNode<T> **> q;
     q.push(&root);
-    for (auto& node : il) {
-        auto** cur = q.front();
+    for (auto &node : il) {
+        auto **cur = q.front();
         q.pop();
         *cur = node.ptr;
         if (*cur) {
@@ -43,4 +41,4 @@ TreeNode<T>* build_tree(std::initializer_list<_Node<T, TreeNode<T>>> il) {
     return root;
 }
 
-#endif // TREE_H
+#endif  // TREE_H
